20a197e99345bed0215686c002b4f16552267c63
gijendrap <144902090+gijendrap@users.noreply.github.com>
2024-09-22 12:09:38 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 
 const pricingPlans = [
